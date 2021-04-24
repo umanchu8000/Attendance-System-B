19f5782ad30199998e8ad9c9214224d6e3d26d64
umanchu8000 <nmk100523091452050750@gmail.com>
2021-04-24 05:05:01 +0000

    Finish editing-users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   
   validates :name, presence: true, length: { maximum: 50 }
+  validates :password,presence: true,length: { minimum:6 },allow_nil: true
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
