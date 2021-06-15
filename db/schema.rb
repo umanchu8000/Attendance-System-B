32402ac5c964b559bac6b54131d757ac619dacf5
umanchu8000 <nmk100523091452050750@gmail.com>
2021-06-15 20:06:05 +0000

    initialize repository

--- db/schema.rb
+++ db/schema.rb
@@ -40,8 +40,8 @@ ActiveRecord::Schema.define(version: 20210518134420) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2021-05-18 23:00:00"
-    t.datetime "work_time", default: "2021-05-18 22:30:00"
+    t.datetime "basic_time", default: "2021-06-14 23:00:00"
+    t.datetime "work_time", default: "2021-06-14 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
